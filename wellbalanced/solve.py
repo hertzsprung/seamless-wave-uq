8d9532557241a397250f1d309b9f5c93e149524d
James Shaw <js102@zepler.net>
2018-08-28 14:53:30 +0100

    add reference well-balanced solver

--- wellbalanced/solve.py
+++ wellbalanced/solve.py
@@ -18,25 +18,29 @@ def topography(x):
     return z*0.5
 
 def topoGradTerm(i):
-    z_iMinusHalf = z[0] if i == 0 else 0.5*(z[i-1]+z[i])
-    z_iPlusHalf = z[N-1] if i == N-1 else 0.5*(z[i]+z[i+1])
-
-    return -g*h[i]*(z_iPlusHalf - z_iMinusHalf)
+    return -g*(h[i]+z[i]-zAverage[i])*(zFace[i+1] - zFace[i])
 
 g = 9.80665
 domain = [0, 50]
-N = 128
+N = 256
 dx = (domain[1] - domain[0])/N
 x = np.linspace(dx/2, domain[1]-dx/2, N)
-endTime = 10.0
-dt = 0.01
+endTime = 100.0
+dt = 0.02
 
 h = np.zeros(N)
 q = np.zeros(N)
 z = np.zeros(N)
 
-zFaces = [topography(x) for x in np.linspace(0, domain[1], N+1)]
-z[:] = [0.5*(l+r) for l, r in zip(zFaces, zFaces[1:])]
+zFace = [topography(x) for x in np.linspace(0, domain[1], N+1)]
+z[:] = [0.5*(l+r) for l, r in zip(zFace, zFace[1:])]
+
+zFace = np.zeros(N+1)
+zFace[0] = z[0]
+zFace[-1] = z[-1]
+zFace[1:-1] = [max(l,r) for l, r in zip(z, z[1:])]
+zAverage = np.zeros(N)
+zAverage[:] = [0.5*(l+r) for l, r in zip(zFace, zFace[1:])]
 
 h = 2.0 - z
 
@@ -44,6 +48,7 @@ riemann = swepc.Roe(g)
 
 f, axes = plt.subplots(2, sharex=True)
 
+c = 0
 t = 0.0
 while t < endTime:
     flux = np.zeros(N+1, dtype=swepc.FlowValue)
@@ -57,7 +62,12 @@ while t < endTime:
             right = left
         else:
             left = swepc.FlowValue(h[i-1], q[i-1])
+            left_h = h[i-1]+z[i-1]-zFace[i]
+            left = swepc.FlowValue(left_h, left.u()*left_h)
+
             right = swepc.FlowValue(h[i], q[i])
+            right_h = h[i]+z[i]-zFace[i]
+            right = swepc.FlowValue(right_h, right.u()*right_h)
 
         flux[i] = riemann.flux(left, right)
 
@@ -66,12 +76,14 @@ while t < endTime:
         q[i] -= dt/dx * ((flux[i+1] - flux[i]).q - topoGradTerm(i))
 
     t += dt
-
-    axes[0].cla()
-    axes[0].annotate("$t$ = {0:.3f}".format(t), (0, 0), xycoords='axes fraction')
-    axes[0].plot(x, z)
-    axes[0].plot(x, z+h)
-    axes[1].cla()
-    axes[1].plot(x, q)
-    plt.draw()
-    plt.pause(0.01)
+    c += 1
+    
+    if c % 8 == 0:
+        axes[0].cla()
+        axes[0].annotate("$t$ = {0:.3f}".format(t), (0, 0), xycoords='axes fraction')
+        axes[0].plot(x, z)
+        axes[0].plot(x, z+h)
+        axes[1].cla()
+        axes[1].plot(x, q)
+        plt.draw()
+        plt.pause(0.01)
