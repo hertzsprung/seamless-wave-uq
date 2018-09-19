050e5e17dbdf19fb95bbd9aa2c789d9f181dd80c
James Shaw <js102@zepler.net>
2018-09-19 11:52:26 +0100

    switch between h-form and eta-form in solve.py

--- swepc.python/solve.py
+++ swepc.python/solve.py
@@ -4,14 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def intrusivePC(initialConditions, boundaryConditions, sourceTerm, degree):
+def initialisePC(initialConditions, boundaryConditions, sourceTerm,
+        deterministicFlux, flowValueClass, degree):
     basis = swepc.GaussianHermiteBasis(degree)
-    riemannSolver = swepc.Roe(swepc.DeterministicFluxEta(g), g)
+    riemannSolver = swepc.Roe(deterministicFlux, g)
     riemannEnsemble = swepc.RiemannEnsemble(
             basis, riemannSolver, quadraturePoints=degree+1)
     flux = swepc.StochasticFlux(basis, riemannEnsemble, sourceTerm)
     flow = swepc.Flow(basis, initialConditions, boundaryConditions,
-            swepc.FlowValueEta)
+            flowValueClass)
 
     return swepc.Simulation(g, flux, sourceTerm), flow
 
@@ -25,11 +26,19 @@ def plotPC():
             pcFlow.z[:,0] + stddev.z, 
             color='lightslategray')
     axarr[0].plot(xCentre, pcFlow.z[:,0], color='k')
+
+#    axarr[0].fill_between(xCentre,
+#            pcFlow.water[:,0] - stddev.water,
+#            pcFlow.water[:,0] + stddev.water, 
+#            color='lightskyblue')
+#    axarr[0].plot(xCentre, pcFlow.water[:,0], color='mediumblue')
+
     axarr[0].fill_between(xCentre,
-            pcFlow.water[:,0] - stddev.water,
-            pcFlow.water[:,0] + stddev.water, 
+            pcFlow.z[:,0] + pcFlow.water[:,0] - stddev.water,
+            pcFlow.z[:,0] + pcFlow.water[:,0] + stddev.water, 
             color='lightskyblue')
-    axarr[0].plot(xCentre, pcFlow.water[:,0], color='mediumblue')
+    axarr[0].plot(xCentre, pcFlow.z[:,0] + pcFlow.water[:,0], color='mediumblue')
+
     axarr[0].annotate("$t$ = {0:.3f}".format(t), (0, 0.9), xycoords='axes fraction')
     axarr[0].annotate("$cfl$ = {0:.3f}".format(
         dt/dx*pcFlowInfo.maxWaveSpeed(pcFlow)), (0, 0.8), xycoords='axes fraction')
@@ -78,14 +87,23 @@ ic = swepc.InitialConditions(N, degree=1)
 zFace = [topography(x) for x in xFace]
 ic.z[:,0] = [0.5*(l+r) for l, r in zip(zFace, zFace[1:])]
 ic.z[:,1] = [0.8/np.sqrt(2*np.pi)*np.exp(-(1.5*(x-10))**2/2) for x in xCentre]
-ic.water[:,0] = 2.0
 
 bc = swepc.BoundaryConditions()
 
-sourceTerm = swepc.WellBalancedEta()
+#sourceTerm = swepc.WellBalancedEta()
+#deterministicFlux = swepc.DeterministicFluxEta(g)
+#flowValueClass = swepc.FlowValueEta
+#pcFlowInfo = swepc.FlowInfoEta(g)
+#ic.water[:,0] = 2.0
 
-pcSim, pcFlow = intrusivePC(ic, bc, sourceTerm, degree=3)
-pcFlowInfo = swepc.FlowInfoEta(g)
+sourceTerm = swepc.WellBalancedH()
+deterministicFlux = swepc.DeterministicFluxH(g)
+flowValueClass = swepc.FlowValueH
+pcFlowInfo = swepc.FlowInfoH(g)
+ic.water[:,0] = [2.0 - z for z in ic.z[:,0]]
+
+pcSim, pcFlow = initialisePC(ic, bc, sourceTerm, deterministicFlux,
+        flowValueClass, degree=3)
 
 _, axarr = plt.subplots(2, figsize=(6,6))
 
